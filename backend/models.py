b94429388b7eb0e18cea40002ab0624d5121c328
Abhijit1024 <abhi.decoder1@gmail.com>
2025-09-02 14:58:55 +0000

    Add requirment

---
+++ backend/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+
+Base = declarative_base()
+
+class URL(Base):
+    __tablename__ = "urls"
+
+    id = Column(Integer, primary_key=True, index=True)
+    short_code = Column(String, unique=True, index=True)
+    original_url = Column(String, nullable=False)
+    click_count = Column(Integer, default=0)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    last_accessed = Column(DateTime, nullable=True)
